ce25a6075a3e43aa047e515aff636dae51088d97
ajimz12 <alvarojimgil12@gmail.com>
2025-01-29 20:07:29 +0100

    ADD: chat

---
+++ chat/src/Form/MessageType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('text', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'rows' => 2,
+                    'placeholder' => 'Write your message...',
+                ],
+            ]);
+    }
+}
